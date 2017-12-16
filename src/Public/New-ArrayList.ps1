1fcefa7e12d865a872bf0016b7e7ab984b5c57e0
Brian Bunke <me@brianbunke.com>
2017-12-16 15:23:29 -0800

    Add help for New-ArrayList

--- src/Public/New-ArrayList.ps1
+++ src/Public/New-ArrayList.ps1
@@ -1,3 +1,23 @@
 ﻿function New-ArrayList {
+    <#
+    .SYNOPSIS
+    Create a new array to append objects into.
+
+    .DESCRIPTION
+    Constructs a new [System.Collections.ArrayList] object.
+
+    Capture New-ArrayList in a variable to reuse it with Add-ArrayObject.
+
+    .EXAMPLE
+    $a = New-ArrayList
+    Create $a as an empty ArrayList, to use later with Add-ArrayObject.
+
+    .OUTPUTS
+    [System.Collections.ArrayList]
+
+    .LINK
+    https://github.com/brianbunke/ArrayList
+    #>
+
     New-Object System.Collections.ArrayList
 }
