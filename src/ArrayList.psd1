3e3ea06869f5770d626d9945145f1a3ef0a90a26
Brian Bunke <me@brianbunke.com>
2017-12-26 05:48:11 -0800

    Add Remove-ArrayObject

--- src/ArrayList.psd1
+++ src/ArrayList.psd1
@@ -12,7 +12,7 @@
 RootModule = 'ArrayList.psm1'
 
 # Version number of this module.
-ModuleVersion = '0.0.1'
+ModuleVersion = '0.1.0'
 
 # Supported PSEditions
 # CompatiblePSEditions = @()
@@ -71,7 +71,8 @@ PowerShellVersion = '3.0'
 # Functions to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no functions to export.
 FunctionsToExport = @(
     'Add-ArrayObject',
-    'New-ArrayList'
+    'New-ArrayList',
+    'Remove-ArrayObject'
 )
 
 # Cmdlets to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no cmdlets to export.
---
+++ src/Public/Remove-ArrayObject.ps1
@@ -0,0 +1,55 @@
+﻿function Remove-ArrayObject {
+    <#
+    .SYNOPSIS
+    
+
+    .DESCRIPTION
+    
+
+    .EXAMPLE
+    
+
+    .LINK
+    https://github.com/brianbunke/ArrayList
+    #>
+    [CmdletBinding(
+        SupportsShouldProcess = $true,
+        ConfirmImpact = 'Medium'
+    )]
+    param (
+        [System.Collections.ArrayList]$Array,
+
+        [Parameter(
+            Mandatory = $true,
+            ValueFromPipeline = $true
+        )]
+        $InputObject
+    )
+
+    BEGIN {
+        $Remove = New-Object System.Collections.ArrayList
+    }
+
+    PROCESS {
+        # Adding to a new array to remove in END block avoids the error:
+        # "Collection was modified; enumeration operation may not execute"
+
+        ForEach ($Object in $InputObject) {
+            [void]$Remove.Add($Object)
+        }
+    } #PROCESS
+
+    END {
+        ForEach ($r in $Remove) {
+            If ($PSCmdlet.ShouldProcess(
+                "$r",
+                'Remove all matching objects from array'
+            )) {
+                # Needed for example: 3 | Remove-ArrayObject
+                While ($r -in $Array) {
+                    $Array.Remove($r)
+                } #While
+            } #WhatIf
+        } #ForEach $r
+    } #END
+}
